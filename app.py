cdbc175ed70c182d0635aa0b03c74162ed05d276
Ahsam Yousaf <ahsamyousaf01@gmail.com>
2025-11-11 11:47:18 -0800

    Flask Routes and JINJA TEMPLATING

--- app.py
+++ app.py
@@ -1,45 +1,56 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def home():
-    return '<h1 style="background-color: yellow;">Hello World!</h1>'
+    new_value= "THIS IS AHSAM'S CODING JOURNEY"
 
-@app.route("/about", methods=["GET", "POST"])
+    return render_template("index.html")
+
+@app.route("/about")
 def about():
+    return render_template("other.html")
 
-    if request.method == "GET":
-        return "GET METHOD CALLED\n"
-    elif request.method == "POST":
-        return "POST METHOD CALLED\n"
-    else:
-        return 'About Us'
+@app.route("/redirect")
+def redirect():
+    return render_template("redirect.html")
 
-@app.route('/contact/<rabta>')
-def contact(rabta):
-    return f"<h1>Contact Us at {rabta} </h1>"
-
-# @app.route('/sum/<int:num1>/<int:num2>')
-@app.route('/sum/<num1>/<num2>')
-def rabta(num1,num2):
-    num1 = int(num1)
-    num2 = int(num2)
-    return f"Sum of {num1} and {num2} = {num1+num2}"
-
-@app.route('/url_params')
-def url_params():
-    name = request.args.get('name')
-    age = request.args.get('age')
-    if name and age:
-        return f"<h1>{name} is {age}</h1>"
-    elif name:
-        return f"<h1>NAME is {name}</h1>"
-    elif age:
-        return f"<h1>AGE {age}</h1>"
-    else:
-        return f"<h1>NAME and AGE are required</h1>"
-
+# @app.route("/about", methods=["GET", "POST"])
+# def about():
+#
+#     if request.method == "GET":
+#         return "GET METHOD CALLED\n"
+#     elif request.method == "POST":
+#         return "POST METHOD CALLED\n"
+#     else:
+#         return 'About Us'
+#
+# @app.route('/contact/<rabta>')
+# def contact(rabta):
+#     return f"<h1>Contact Us at {rabta} </h1>"
+#
+# # @app.route('/sum/<int:num1>/<int:num2>')
+# # route parameters
+# @app.route('/sum/<num1>/<num2>')
+# def rabta(num1,num2):
+#     num1 = int(num1)
+#     num2 = int(num2)
+#     return f"Sum of {num1} and {num2} = {num1+num2}"
+#
+# @app.route('/url_params')
+# def url_params():
+#     name = request.args.get('name')
+#     age = request.args.get('age')
+#     if name and age:
+#         return f"<h1>{name} is {age}</h1>"
+#     elif name:
+#         return f"<h1>NAME is {name}</h1>"
+#     elif age:
+#         return f"<h1>AGE {age}</h1>"
+#     else:
+#         return f"<h1>NAME and AGE are required</h1>"
+#
 
 # if app.name == "__main__":
 
