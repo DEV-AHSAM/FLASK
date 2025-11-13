70cd30c60512341e8c9ceb4cb96a1b5949b8df86
Ahsam Yousaf <ahsamyousaf01@gmail.com>
2025-11-13 12:35:25 -0800

    NOV 13 did nothing but just keeping the consistency by pushing the code and watch the tutorial

--- app.py
+++ app.py
@@ -5,55 +5,55 @@ import io
 
 app = Flask(__name__, template_folder="templates")
 
-# @app.route("/")
-# def home():
-#     new_value= "THIS IS AHSAM'S CODING JOURNEY"
-#
-#     return render_template("index.html")
-#
-# @app.route("/about")
-# def about():
-#     return render_template("other.html")
+@app.route("/")
+def home():
+    new_value= "THIS IS AHSAM'S CODING JOURNEY"
+
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("other.html")
 #
 @app.route("/redirect")
 def redirect():
     return render_template("redirect.html")
 
-# @app.route("/about", methods=["GET", "POST"])
-# def about():
-#
-#     if request.method == "GET":
-#         return "GET METHOD CALLED\n"
-#     elif request.method == "POST":
-#         return "POST METHOD CALLED\n"
-#     else:
-#         return 'About Us'
-#
-# @app.route('/contact/<rabta>')
-# def contact(rabta):
-#     return f"<h1>Contact Us at {rabta} </h1>"
-#
-# # @app.route('/sum/<int:num1>/<int:num2>')
-# # route parameters
-# @app.route('/sum/<num1>/<num2>')
-# def rabta(num1,num2):
-#     num1 = int(num1)
-#     num2 = int(num2)
-#     return f"Sum of {num1} and {num2} = {num1+num2}"
-#
-# @app.route('/url_params')
-# def url_params():
-#     name = request.args.get('name')
-#     age = request.args.get('age')
-#     if name and age:
-#         return f"<h1>{name} is {age}</h1>"
-#     elif name:
-#         return f"<h1>NAME is {name}</h1>"
-#     elif age:
-#         return f"<h1>AGE {age}</h1>"
-#     else:
-#         return f"<h1>NAME and AGE are required</h1>"
-#
+@app.route("/about", methods=["GET", "POST"])
+def about():
+
+    if request.method == "GET":
+        return "GET METHOD CALLED\n"
+    elif request.method == "POST":
+        return "POST METHOD CALLED\n"
+    else:
+        return 'About Us'
+
+@app.route('/contact/<rabta>')
+def contact(rabta):
+    return f"<h1>Contact Us at {rabta} </h1>"
+
+# @app.route('/sum/<int:num1>/<int:num2>')
+# route parameters
+@app.route('/sum/<num1>/<num2>')
+def rabta(num1,num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return f"Sum of {num1} and {num2} = {num1+num2}"
+
+@app.route('/url_params')
+def url_params():
+    name = request.args.get('name')
+    age = request.args.get('age')
+    if name and age:
+        return f"<h1>{name} is {age}</h1>"
+    elif name:
+        return f"<h1>NAME is {name}</h1>"
+    elif age:
+        return f"<h1>AGE {age}</h1>"
+    else:
+        return f"<h1>NAME and AGE are required</h1>"
+
 
 
 @app.route("/login", methods=["GET", "POST"])
